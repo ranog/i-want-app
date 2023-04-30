1e2a36f3597059ee9dc97fd0a3a6bfdc0f247df3
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-04-30 20:59:49 -0300

    60. Creating product domain

--- src/Domain/Products/Product.cs
+++ src/Domain/Products/Product.cs
@@ -1,9 +1,12 @@
 namespace IWantApp.Domain.Products;
 
-public class Category
+public class Product
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public Category Category { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
     public string CreatedBy { get; set; }
     public DateTime CreatedOn { get; set; }
     public string EditedBy { get; set; }
