c445871e83ea299268f20e2664dc891924bfb078
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-04-30 21:50:01 -0300

    64. EF context

--- src/Domain/Products/Product.cs
+++ src/Domain/Products/Product.cs
@@ -3,6 +3,7 @@ namespace IWantApp.Domain.Products;
 public class Product : Entity
 {
     public string Name { get; set; }
+    public int CategoryId { get; set; }
     public Category Category { get; set; }
     public string Description { get; set; }
 }
