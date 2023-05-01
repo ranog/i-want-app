b45ddbcc75913409156c19fc5639fee7e56588ae
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-04-30 21:11:30 -0300

    62. Refactoring domain

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -1,11 +1,6 @@
 namespace IWantApp.Domain.Products;
 
-public class Category
+public class Category : Entity
 {
-    public Guid Id { get; set; }
     public string Name { get; set; }
-    public string CreatedBy { get; set; }
-    public DateTime CreatedOn { get; set; }
-    public string EditedBy { get; set; }
-    public DateTime EditedOn { get; set; }
 }
