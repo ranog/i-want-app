b785b7a377d3363eb57b145b853b2baa66e03575
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-01 00:10:33 -0300

    67. Generating and applying migrations

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,9 @@
 using IWantApp.Endpoints.Categories;
+using IWantApp.Infra.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
