4dedfc895354acf61e344928f7c0df70962ded42
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-02 15:36:58 -0300

    75. Using C# extension methods

--- src/Endpoints/Categories/CategoryPost.cs
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -12,17 +12,13 @@ public class CategoryPost
     public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
     {
         var category = new Category(categoryRequest.Name, "Test", "Test");
-        if (!category.IsValid)
+        if (category.IsValid)
         {
-            var errors = category.Notifications
-                .GroupBy(g => g.Key)
-                .ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
-            return Results.ValidationProblem(errors);
+            context.Categories.Add(category);
+            context.SaveChanges();
+            
+            return Results.Created($"{Template}/{category.Id}", category.Id);
         }
-
-        context.Categories.Add(category);
-        context.SaveChanges();
-
-        return Results.Created($"{Template}/{category.Id}", category.Id);
+        return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
     }
 }
