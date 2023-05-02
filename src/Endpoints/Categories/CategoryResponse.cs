32daafa9c458156601167ec1260139643b78ed91
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-02 11:54:42 -0300

    69. Other category endpoints

--- src/Endpoints/Categories/CategoryResponse.cs
+++ src/Endpoints/Categories/CategoryResponse.cs
@@ -1,7 +1,8 @@
 namespace IWantApp.Endpoints.Categories;
 
-public class CategoryRequest
+public class CategoryResponse
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public bool Active { get; set; }
 }
