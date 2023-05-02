fdd4fb14531cf8760016d8d682c8959066cbb25e
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-02 15:21:38 -0300

    74. Problem details

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -11,6 +11,7 @@ public class Category : Entity
     {
         var contract = new Contract<Category>()
             .IsNotNullOrEmpty(name, "Name")
+            .IsGreaterOrEqualsThan(name, 3, "Name")
             .IsNotNullOrEmpty(createdBy, "CreatedBy")
             .IsNotNullOrEmpty(editedBy, "EditedBy");
         AddNotifications(contract);
--- src/Endpoints/Categories/CategoryPost.cs
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -13,7 +13,12 @@ public class CategoryPost
     {
         var category = new Category(categoryRequest.Name, "Test", "Test");
         if (!category.IsValid)
-            return Results.BadRequest(category.Notifications);
+        {
+            var errors = category.Notifications
+                .GroupBy(g => g.Key)
+                .ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
+            return Results.ValidationProblem(errors);
+        }
 
         context.Categories.Add(category);
         context.SaveChanges();
