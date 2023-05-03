b5c41466003bf9ce1be237c18dad4cf94ad86353
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-03 16:20:03 -0300

    82. Saving claims list

--- src/Endpoints/Employees/EmployeePost.cs
+++ src/Endpoints/Employees/EmployeePost.cs
@@ -19,12 +19,13 @@ public class EmployeePost
         if (!result.Succeeded)
             return Results.BadRequest(result.Errors.First());
 
-        var claimResult =
-            userManager.AddClaimAsync(user, new Claim("EmployeeCode", employeeRequest.EmployeeCode)).Result;
-        if (!claimResult.Succeeded)
-            return Results.BadRequest(result.Errors.First());
+        var userClaims = new List<Claim>
+        {
+            new Claim("EmployeeCode", employeeRequest.EmployeeCode),
+            new Claim("Name", employeeRequest.Name)
+        };
 
-        claimResult = userManager.AddClaimAsync(user, new Claim("Name", employeeRequest.Name)).Result;
+        var claimResult = userManager.AddClaimsAsync(user, userClaims).Result;
         if (!claimResult.Succeeded)
             return Results.BadRequest(result.Errors.First());
 
