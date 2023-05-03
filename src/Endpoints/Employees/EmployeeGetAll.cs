0272ce85de4847fecc89edb5c598fef310a203e7
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-03 17:16:44 -0300

    84. Querying users

---
+++ src/Endpoints/Employees/EmployeeGetAll.cs
@@ -0,0 +1,26 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+namespace IWantApp.Endpoints.Employees;
+
+public class EmployeeGetAll
+{
+    public static string Template => "/employees";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(UserManager<IdentityUser> userManager)
+    {
+        var users = userManager.Users.ToList();
+        var employees = new List<EmployeeResponse>();
+        foreach (var item in users)
+        {
+            var claims = userManager.GetClaimsAsync(item).Result;
+            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
+            var userName = claimName != null ? claimName.Value : string.Empty;
+            employees.Add(new EmployeeResponse(item.Email, userName));
+        }
+
+        return Results.Ok(employees);
+    }
+}
