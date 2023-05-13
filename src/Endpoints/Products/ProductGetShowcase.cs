bc40bddfa40e98957afb5beeaed9dcff61734062
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-13 01:30:44 -0300

    115. Pagination

--- src/Endpoints/Products/ProductGetShowcase.cs
+++ src/Endpoints/Products/ProductGetShowcase.cs
@@ -7,13 +7,23 @@ public class ProductGetShowcase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static async Task<IResult> Action(ApplicationDbContext context)
+    public static async Task<IResult> Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
     {
-        var products = context.Products
+        page ??= 1;
+        row ??= 10;
+        if (string.IsNullOrEmpty(orderBy))
+            orderBy = "name";
+
+        var queryBase = context.Products
             .Include(p => p.Category)
-            .Where(p => p.HasStock && p.Category.Active)
-            .OrderBy(p => p.Name)
-            .ToList();
+            .Where(p => p.HasStock && p.Category.Active);
+
+        queryBase = orderBy == "name" ? queryBase.OrderBy(p => p.Name) : queryBase.OrderBy(p => p.Price);
+
+        var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+
+        var products = queryFilter.ToList();
+
         var response = products
             .Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active));
         return Results.Ok(response);
